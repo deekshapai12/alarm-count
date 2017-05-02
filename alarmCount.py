644786dff3287857b198f6778bb969d0eea33b30
deekshapai12 <deeksharpai1995@gmail.com>
2017-05-02 12:46:26 +0530

    Update alarmCount.py

--- alarmCount.py
+++ alarmCount.py
@@ -35,52 +35,7 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def text2int(textnum, numwords={}):
-    if not numwords:
-        units = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen",
-      ]
-        tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-        scales = ["hundred", "thousand", "million", "billion", "trillion"]
-        numwords["and"] = (1, 0)
-        for idx, word in enumerate(units):    numwords[word] = (1, idx)
-        for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
-        for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
-    current = result = 0
-    for word in textnum.split():
-        scale, increment = numwords[word]
-        current = current * scale + increment
-        if scale > 100:
-            result += current
-            current = 0
-    return result + current
 
-def ord2int(textnum):
-    ordinal={
-        'first':1,
-        'second':2,
-        'third':3,
-        'fourth':4,
-        'fifth':5,
-        'sixth':6,
-        'seventh':7,
-        'eighth':8,
-        'ninth':9,
-        'tenth':10,
-        'eleventh':11,
-        'twelfth':12,
-        'thirteenth':13,
-        'fourteenth':14,
-        'fifteenth':15,
-        'sixteenth':16,
-        'seventeenth':17,
-        'eighteenth':18,
-        'nineteenth':19,
-        'twentieth':20,
-    }
-    return ordinal[textnum]
 
 def processRequest(req):
     #baseurl = Request("http://52bcca08.ngrok.io/na")
@@ -99,66 +54,11 @@ def processRequest(req):
     # responses = req.get("result").get("responses")
     # print("responses : ")
     # print(responses[0].value)
-    data = filterResult(data,parameters)
+    #data = filterResult(data,parameters)
     actionName = req.get("result").get("action")
     res = makeWebhookResult(actionName,data,parameters)
     return res
 
-
-def filterResult(data,parameters):
-    sourceState = parameters.get("source-state")
-    ackState = parameters.get("ack-state")
-    priority = parameters.get("priority")
-    numberWord = parameters.get("number")
-    ordinal = parameters.get("ordinal")
-    if sourceState == '' and ackState == '' and priority == '':
-        return data
-    result = []
-    if sourceState != '':
-        for alarm in data.get('alarms'):
-            if alarm.get("Source State").lower() == sourceState.lower():
-                result.append(alarm)
-        data['alarms'] = result
-    result = []
-    if ackState != '':
-        for alarm in data.get('alarms'):
-            if alarm.get("Ack State").lower() == ackState.lower():
-                result.append(alarm)
-        data['alarms'] = result
-    result = []
-    if priority != '':
-        for alarm in data.get('alarms'):
-            if alarm.get("Alarm Class").split('_')[0].lower().startswith(priority.split()[0].lower()):
-                result.append(alarm)
-        data['alarms'] = result
-    result = []
-    if numberWord != '':
-        if numberWord.lower() == 'one':
-            if ordinal == '' or ordinal == 'first':
-                result = data.get('alarms')[0]
-            elif ordinal.lower() == 'last':
-                result = data.get('alarms')[-1]
-            else:
-                number = ord2int(ordinal)
-                data['alarms'] = data.get('alarms')[number]
-        else:
-            number = text2int(numberWord)
-            if ordinal == '':
-                if number < len(data):
-                    data['alarms'] = data.get('alarms')[number-1]
-            elif ordinal.lower() == 'first':
-                if number < len(data):
-                    data['alarms'] = data.get('alarms')[0:number]
-            elif ordinal.lower() == 'last':
-                if number < len(data):
-                    data['alarms'] = data.get('alarms')[number-1:len(data)]
-    elif ordinal != '':
-        if ordinal == 'last' or ordinal == 'latest':
-            data['alarms'] = data.get('alarms')[-1]
-        else:
-            number = ord2int(ordinal)
-            data['alarms'] = data.get('alarms')[number-1]
-    return data
 
 
 
