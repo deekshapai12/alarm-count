8829411eabd6dcfa9eb68dd7b872fc2031961ae0
deekshapai12 <deeksharpai1995@gmail.com>
2017-05-02 16:51:53 +0530

    Update alarmCount.py

--- alarmCount.py
+++ alarmCount.py
@@ -38,15 +38,7 @@ def webhook():
 
 
 def processRequest(req):
-    #baseurl = Request("http://52bcca08.ngrok.io/na")
-    #baseurl.add_header("Authorization","Basic R0h0ZXN0OlRyaWRpdW0xMjM=")
-    #post_fields = {'requests':[{'message':'GetRollup','node':'station slot:/TestPoints/Bangalore','data':'n:history','timeRange':'today','rollup':'sum'}]}
-    #print("Firing request for data")
-    #r=urlopen(baseurl, json.dumps(post_fields))
-    #result=r.read().decode()
-   # result = urlopen(baseurl).read().decode()
     url = "http://52bcca08.ngrok.io/na"
-
     payload = "{\"requests\":[{\"message\":\"GetRollup\",\"node\":\"slot:/TestPoints/Bangalore\",\"data\":\"n:history\",\"timeRange\":\"today\",\"rollup\":\"sum\"}]}"
     headers = {
         'authorization': 'Basic R0h0ZXN0OlRyaWRpdW0xMjM=',
@@ -55,104 +47,21 @@ def processRequest(req):
         }
 
     response = requests.request("POST", url, data=payload, headers=headers)
-    
-    print("Response is :")
-    print(response.text)
-    print("Data is :")
     data= json.loads(response.text)
-    print(data)
     actionName = req.get("result").get("action")
-    res = makeWebhookResult(actionName,data,parameters)
+    res = makeSpeechResponse(actionName,data)
     return res
 
-
-
-
-def makeWebhookResult(actionName,data,parameters):
-    alarms = data.get('alarms')
-    if alarms is None:
-        return {}
-    # print(json.dumps(item, indent=4))
-    sourceState = parameters.get("source-state")
-    ackState = parameters.get("ack-state")
-    priority = parameters.get("priority")
-    numberWord = parameters.get("number")
-    ordinal = parameters.get("ordinal")
-    if actionName == "alarmCount":
-        speech = "There"
-        count = len(alarms)
-        if count == 1:
-            speech+=" is 1 alarm"
-        else:
-            speech+=" are "+str(count)+" alarms"
-        if not sourceState and not ackState and not priority:
-            speech+=" in total"
-        else:
-            speech+=" having "
-            joint = " "
-            count = 0
-            if sourceState:
-                if count>0:
-                    joint=" and "
-                speech+=joint+"source state as "+sourceState.upper()
-                count+=1
-            if ackState:
-                if count>0:
-                    joint=" and "
-                speech+=joint+"acknowledgement state as "+ackState.upper()
-                count+=1
-            if priority:
-                if count>0:
-                    joint=" and "
-                speech+=joint+"priority as "+priority.split()[0].upper()
-                count+=1
-
-    elif actionName == "fixAlarms":
-        speech = "The following steps need to be followed to fix the "
-        if ordinal != '':
-            speech+=ordinal
-        speech += " alarm "
-        if numberWord != '' :
-            speech+=numberWord+" "
-        speech+='having '
-        joint = " "
-        count = 0
-        if sourceState:
-            if count>0:
-                joint=" and "
-            speech+=joint+"source state as "+sourceState.upper()
-            count+=1
-        if ackState:
-            if count>0:
-                joint=" and "
-            speech+=joint+"acknowledgement state as "+ackState.upper()
-            count+=1
-        if priority:
-            if count>0:
-                joint=" and "
-            speech+=joint+"priority as "+priority.split()[0].upper()
-            count+=1
-        speech+=" "+data.get("alarms")[0].get("fix")
-
-    elif actionName == "getAlarm" or actionName == "getLatestAlarm":
-        alarm = data.get("alarms")[0]
-        ack = alarm.get("Ack State")
-        if ack.beginswith("un"):
-            ack="unacknowledged"
-        else:
-            ack="acknowledged"
-        priority_class=" ".join(alarm.get("Alarm Class").split("_")[:2])
-        speech="This alarm is "+alarm.get("Source State")+", "+ack+" and "+priority_class+" with a priority of "+alarm.get("Priority")
-
-    # print("Response:")
-    # print(speech)
-
+def makeSpeechResponse(actionName,data):
+    if actionName == "totalEnergy":
+        
+    total = data.["responses"][0]["value"]
+    total = math.ceil(float(total))
+    speech = "The total energy consumption for Bangalore orion campus today is " + total
     return {
         "speech": speech,
         "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "random JSON data"
+        "source": "Niagara"
     }
 
 if __name__ == '__main__':
