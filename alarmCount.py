a1423e5b11d50ea9fbf44607f6abcefefb7d4235
deekshapai12 <deeksharpai1995@gmail.com>
2017-05-10 14:59:36 +0530

    Update alarmCount.py

--- alarmCount.py
+++ alarmCount.py
@@ -50,9 +50,10 @@ def processRequest(req):
         payload = "{\n\taction : \"getAllAlarmListCount\",\n\tparams : []\n}"
     elif actionName == "allCriticalAlarms":
         payload = "{action : \"getAllCriticalAlarms\",params : []}"
-    elif actionName == "alarmInstructions-yes":
-        alarmIndex = req.get("result").get("contexts")[0].get("parameters").get("alarmIndex")
-#         alarmIndex = req.get("result").get("parameters").get("alarmIndex")
+    #elif actionName == "alarmInstructions-yes":
+        #alarmIndex = req.get("result").get("contexts")[0].get("parameters").get("alarmIndex")
+    elif actionName == "alarmInstructions
+        alarmIndex = req.get("result").get("parameters").get("alarmIndex")
         payload = "{action : \"getAlarmInstruction\",params : [" + alarmIndex + "]}"
     elif actionName == "similarAlarm":
         payload = "{action : \"getSimilarAlarms\",params : []}"
