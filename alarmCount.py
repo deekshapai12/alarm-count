eea83218efea48c3dddff545d96c847243e200f5
deekshapai12 <deeksharpai1995@gmail.com>
2017-05-16 15:10:11 +0530

    Update alarmCount.py

--- alarmCount.py
+++ alarmCount.py
@@ -224,8 +224,8 @@ def makeSpeechResponse(actionName,topicName,subscriptionName):
            }
 
 if __name__ == '__main__':
-    print("Hello , Hi, start")
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='Deeksha Project-5204d514b418.json'
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="Deeksha Project-5204d514b418.json"
+    print os.environ("GOOGLE_APPLICATION_CREDENTIALS")
     #scopes = ['https://www.googleapis.com/auth/sqlservice.admin']
     #credentials = ServiceAccountCredentials.from_json_keyfile_name('Deeksha Project-5204d514b418.json', scopes=scopes)
     port = int(os.getenv('PORT', 5000))
